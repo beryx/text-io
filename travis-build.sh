726cd5ce4093201b3f75ad7aa113595926f034ae
Serban Iordache <iordache@hotmail.com>
2016-11-10 00:18:13 +0100

    Update travis-build.sh

--- travis-build.sh
+++ travis-build.sh
@@ -4,8 +4,8 @@ set -ev
 
 if [ "${TRAVIS_PULL_REQUEST}" == "false" -a "${TRAVIS_BRANCH}" == "master" ]; then
   if [ "`git ls-remote origin gh-pages`" == "" ]; then
-    ./gradlew publishGhPages --rerun-tasks -PghPageType=init
+    ./gradlew publishGhPages --info --stacktrace --rerun-tasks -PghPageType=init
   fi
-  ./gradlew publishGhPages --rerun-tasks -PghPageType=latest
-  ./gradlew publishGhPages --rerun-tasks -PghPageType=version
+  ./gradlew publishGhPages --info --stacktrace --rerun-tasks -PghPageType=latest
+  ./gradlew publishGhPages --info --stacktrace --rerun-tasks -PghPageType=version
 fi
