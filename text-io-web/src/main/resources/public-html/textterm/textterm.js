cb01a6e9ebed36f3b57937401fe5df29a986b443
Serban Iordache <iordache@hotmail.com>
2017-07-09 13:26:44 +0200

    reset action value on execute and restart

--- text-io-web/src/main/resources/public-html/textterm/textterm.js
+++ text-io-web/src/main/resources/public-html/textterm/textterm.js
@@ -1,4 +1,4 @@
-/*
+ /*
  * Copyright 2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -261,6 +261,7 @@
         var currentInitData = null;
 
         var postInitData = function(initData) {
+            action = undefined;
             currentInitData = initData;
             var xhr = new XMLHttpRequest();
             xhr.onreadystatechange = handleXhrStateChange(xhr);
